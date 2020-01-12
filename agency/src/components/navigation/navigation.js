485deaaddfed793790ee0250c0e15d03bd1e074c
MagdalenaJasinska <m.wronska1993@gmail.com>
2020-01-12 19:49:17 +0100

    add onscroll functionality

--- agency/src/components/navigation/navigation.js
+++ agency/src/components/navigation/navigation.js
@@ -1,13 +1,50 @@
 import React from 'react';
 import './navigation.css';
 
+let lastScrollY = 0;
+
+
 class Navigation extends React.Component {
+
+state ={
+   scrollAction: false,
+}
+
+componentDidMount () {
+    window.addEventListener('scroll', this.handleScroll);
+}
+    
+componentWillUnmount () {
+    window.removeEventListener('scroll', this.handleScroll);
+}
+
+handleScroll = () => {
+    lastScrollY = window.scrollY;
+    if (lastScrollY >= 120) {
+        this.setState({
+            scrollAction: true
+        });
+    }
+      else  {
+          this.setState({
+              scrollAction: false
+          });
+      }
+}
     render () {
         return (
-            <nav>
+            < nav className = {
+                this.state.scrollAction ? 'scroll_action' : null
+            }
+            >
                
                 <ul>
-                <li className="logo">Company logo</li>
+                <li className= {
+                this.state.scrollAction ? 'logo scroll_action_logo' : 'logo'
+            }
+                >
+                    Company logo
+                </li>
                 <li>Services</li>
                 <li>Portfolio</li>
                 <li>About</li>
