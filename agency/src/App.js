24f30f59c3b040a8dfb7a1b644aae0f83aaec14c
MagdalenaJasinska <m.wronska1993@gmail.com>
2020-02-03 21:10:34 +0100

    create hello

--- agency/src/App.js
+++ agency/src/App.js
@@ -7,10 +7,12 @@ import Footer from './components/footer/footer';
 import './App.css';
 import Navigation from './components/navigation/navigation';
 import Portfolio from './components/body/portfolio/portfolio';
+import Hello from './components/navigation/hello';
 
 function App() {
   return (
     <div className="App">
+      <Hello/>
       <Navigation/>  
       <Header/>
       <Services/>
---
+++ agency/src/components/navigation/hello.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './hello.css';
+
+
+class Hello extends React.Component {
+
+constructor(props) {
+    super(props);
+    this.state= {
+        isVisible: true
+    }
+}
+
+componentDidMount() {
+    this.hideSaveCover ();
+}
+
+componentWillUnmount () {
+    clearTimeout(this.timer);
+}
+
+  hideSaveCover() {
+    
+    this.timer = setTimeout(() => {
+      this.setState({
+        isVisible: false
+      });
+    }, 2300);
+  }
+  
+
+  render() {
+    return (
+      <div className="hello" style={{ display: this.state.isVisible ? "flex" : "none"}}>
+       <p className="hello_txt"> Hello </p>
+      </div>
+    );
+  }
+
+}
+
+export default Hello;
