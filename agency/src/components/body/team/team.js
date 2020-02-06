df632ab3a06248d4c10f025ebd6e14e391d5b033
MagdalenaJasinska <m.wronska1993@gmail.com>
2020-02-06 20:35:45 +0100

    crate team section

--- agency/src/components/body/team/team.js
+++ agency/src/components/body/team/team.js
@@ -34,10 +34,30 @@ class Team extends React.Component {
     }
 
     render() {
-        console.log(this.state.items)
 
-        this.state.items.forEach((item) => {
-            console.log(item.name.first, item.name.last, item.email)
+        let itemsHtml = [];
+        
+
+        this.state.items.forEach((item, i) => {
+           
+            itemsHtml.push(
+            <div className="team_member" key={"member"+i} >
+                <img className="team_member_image" src={item.picture.large} alt="team member" />
+                <p className="team_member_name">{item.name.first} {item.name.last}</p>
+                <p className="team_member_email">{item.email}</p>
+                <div className="socialsIcons_container">
+                    <span className="item_icon_container">
+                        <FontAwesomeIcon className="item_icon social" icon={faTwitter} />
+                    </span>
+                    <span className="item_icon_container">
+                        <FontAwesomeIcon className="item_icon social" icon={faFacebookF} />
+                    </span>
+                    <span className="item_icon_container">
+                        <FontAwesomeIcon className="item_icon social" icon={faLinkedinIn} />
+                    </span>
+                </div>
+            </div>);
+
         })
 
         return (
@@ -49,54 +69,7 @@ class Team extends React.Component {
                         <p className="team_subtittle">Lorem ipsum dolor sit amet consectetur.</p>
                     </div>
                     <div className="team_members_section">
-                        <div className="team_member">
-                            <img className="team_member_image" src="./images/about_images/1.jpg" alt="team member" />
-                            <p className="team_member_name">kay garland</p>
-                            <p className="team_member_position">Lead Designer</p>
-                            <div className="socialsIcons_container">
-                                <span className="item_icon_container">
-                                    <FontAwesomeIcon className="item_icon social" icon={faTwitter} />
-                                </span>
-                                <span className="item_icon_container">
-                                    <FontAwesomeIcon className="item_icon social" icon={faFacebookF} />
-                                </span>
-                                <span className="item_icon_container">
-                                    <FontAwesomeIcon className="item_icon social" icon={faLinkedinIn} />
-                                </span>
-                            </div>
-                        </div>
-                        <div className="team_member">
-                            <img className="team_member_image" src="./images/about_images/1.jpg" alt="team member" />
-                            <p className="team_member_name">kay garland</p>
-                            <p className="team_member_position">Lead Designer</p>
-                            <div className="socialsIcons_container">
-                                <span className="item_icon_container">
-                                    <FontAwesomeIcon className="item_icon social" icon={faTwitter} />
-                                </span>
-                                <span className="item_icon_container">
-                                    <FontAwesomeIcon className="item_icon social" icon={faFacebookF} />
-                                </span>
-                                <span className="item_icon_container">
-                                    <FontAwesomeIcon className="item_icon social" icon={faLinkedinIn} />
-                                </span>
-                            </div>
-                        </div>
-                        <div className="team_member">
-                            <img className="team_member_image" src="./images/about_images/1.jpg" alt="team member" />
-                            <p className="team_member_name">kay garland</p>
-                            <p className="team_member_position">Lead Designer</p>
-                            <div className="socialsIcons_container">
-                                <span className="item_icon_container">
-                                    <FontAwesomeIcon className="item_icon social" icon={faTwitter} />
-                                </span>
-                                <span className="item_icon_container">
-                                    <FontAwesomeIcon className="item_icon social" icon={faFacebookF} />
-                                </span>
-                                <span className="item_icon_container">
-                                    <FontAwesomeIcon className="item_icon social" icon={faLinkedinIn} />
-                                </span>
-                            </div>
-                        </div>
+                        {itemsHtml}
                     </div>
 
                 </div>
